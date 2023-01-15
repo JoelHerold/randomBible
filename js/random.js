cf4252b6e78d6556cf6d30320f1e862ba2d7cf50
JoelHerold <86368518+JoelHerold@users.noreply.github.com>
2023-01-15 19:43:26 +0100

    a

---
+++ js/random.js
@@ -0,0 +1,8 @@
+
+listBocks = ["1Mose","2Mose","3Mose","4Mose","Josua","Richter","Ruth","1Samuel","2Samuel","1Könige","2Könige","1Chronik","2Chronik","Esra","" +
+"Nehemia","Esther","Hiob","Psalme","Sprüche","Prediger","HohesLied","Jesaja","Jeremia","Klagelieder","Hesekiel","Daniel","Hosea",
+"Joel","Amos","Obadja","Jona","Micha","Nahum","Habakuk","Zevanja","Haggai","Sacharja","Maleachi","Matthäus","Markus","Lukas","Johannes","Apostelgeschichte","" +
+    "Römer","1Korinther","2Korinther","Galater","Epheser","Philipper","Kolosser","1Tessalonicher","2Tessalochnicher","1Timotheus","2Timotheus","Titus",
+"Philemon","Hebräer","Jakobus","1Petrus","2Petrus","1Johannes","2Johannes","3Johannes","Judas","Offenbarung"];
+
+console.log(listBocks)
